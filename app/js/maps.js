deb73c3ae53fd89b27be5fdf6f165a8268af527b
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-04-11 03:45:01 -0500

    Add directions to the map.

--- app/js/maps.js
+++ app/js/maps.js
@@ -1,4 +1,9 @@
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+
  function initialize() {
+ 		
+ 		directionsDisplay = new google.maps.DirectionsRenderer();
 
  		var myLatlng = new google.maps.LatLng(36.067386,-94.156918);
         var mapOptions = {
@@ -8,7 +13,7 @@
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"),
             mapOptions);
-
+        directionsDisplay.setMap(map);
         var image = 'img/packet-icon1.png';
 
         var marker1 = new google.maps.Marker({
@@ -39,7 +44,7 @@
 	      '<div id="bodyContent">'+
 	      '<p><b>PRICE:</b>  <b>$99.00</b>'+
 	      '<p><b>SOURCE ADRESS:</b>  <b>1, University of Arkansas</b>'+
-	      '<p><b>DESTINATION ADRESS:</b>  <b>Walmart</b>'+
+	      '<p><b>DESTINATION ADRESS:</b>  <b>Southwest 300th Road, KS</b>'+
 	      '<p><b>DISTANCE:</b>  <b>50 miles</b></br>'+
 	      '<button class="btn btn-primary">GET IT</button>'+
 	      '</div>'+
@@ -51,8 +56,29 @@
 	  });
 
 	  google.maps.event.addListener(marker1, 'click', function() {
+    	calcRoute();
     	infowindow.open(map,marker1);
+
   	});
 
-
  }
+
+function calcRoute() {
+  //var start = document.getElementById('start').value;
+  //var end = document.getElementById('end').value;
+
+  console.log("hello");
+  var start = new google.maps.LatLng(36.067386,-94.156918);
+  var end = new google.maps.LatLng(38.067386,-95.156918);
+
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
